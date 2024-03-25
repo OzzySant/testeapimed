bd1b0b0550c9dfeb7030f0b01f8824b30e488eef
Ozeias Santos <ozzyton18@gmail.com>
2024-03-25 15:30:43 -0300

    Commit inicial

---
+++ APIMED/APIMED/Configuracao/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using APIMED.Businnes.DTO;
+using APIMED.Businnes.ViewModel;
+using APIMED.Domain.Entities;
+using AutoMapper;
+
+namespace APIMED.Application.Configuracao
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Contato, ContatoViewModel>().ReverseMap();
+            CreateMap<ContatoDTO, ContatoViewModel>().ReverseMap();
+        }
+    }
+}
